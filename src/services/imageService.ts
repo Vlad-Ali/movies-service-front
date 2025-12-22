bfcbf35445960dc13ea5488dac2ae23a035f5233
Vlad_Ali <marknext592@gmail.com>
2025-12-22 15:53:01 +0300

    Update imageService.ts

--- src/services/imageService.ts
+++ src/services/imageService.ts
@@ -1,6 +1,6 @@
 export const imageService = {
     getMovieImage(title: string){
         const imageName = title.toLowerCase().replace(/[^a-z0-9]/g, '-');
-        return `/images/movies/${imageName}.jpg`;
+        return `/movies-service-front/images/movies/${imageName}.jpg`;
     }
 };
